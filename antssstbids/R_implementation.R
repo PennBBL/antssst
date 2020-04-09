dd90e44cc071b751e62b4e68ae5a1fd1d27e56d4
Ellyn Butler <butellyn@MacBook-Pro.local>
2020-04-09 14:19:22 -0400

    Loading sessions

--- antssstbids/R_implementation.R
+++ antssstbids/R_implementation.R
@@ -7,6 +7,7 @@
 ### April 9, 2020
 
 library('ANTsR')
+library('RNifti')
 
 
 ##### Read command line arguments to this script #####
@@ -25,9 +26,23 @@ if (length(args) != 2) {
 }
 
 raw_t1w_images <- system(paste0("find ", bidsdir, subj, " ", "-name ", "sub-*_ses-*_T1w.nii.gz"), intern=TRUE)
+if (length(raw_t1w_images < 2) { stop("You must have at least two T1w images", call.=FALSE) }
 
 
-##### Load the images into R #####
+##### Load the images into R, naming images by session label #####
+
+for (t1w_nifti in raw_t1w_images) {
+  seslabel <- strsplit(t1w_nifti, "/")[[1]][length(strsplit(t1w_nifti, "/")[[1]])]
+  seslabel <- strsplit(strsplit(seslabel, "_")[[1]][2], "-")[[1]][2]
+  assign( , readNifti(raw_t1w_images[1]))
+}
+
+
+##### Process data #####
+
+# 1.) N4 bias field correction
+
+
 
 
 
