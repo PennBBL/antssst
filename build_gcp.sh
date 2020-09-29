9decda4c0b526a4bb587780a5f33eee682133dd0
butellyn <ellynrbutler@gmail.com>
2020-09-29 13:32:34 -0400

    Ditch flywheel

--- build_gcp.sh
+++
@@ -1,6 +0,0 @@
-# After merging master, run this script to build and upload the hpc-version
-IMAGENAME=$(cat manifest.json | grep \"image\": | sed 's/^.*"image": "\(.*\)".*/\1/')
-docker build -t ${IMAGENAME} .
-fw gear upload
-docker push $IMAGENAME
-echo "Don't forget to PR the gear exchange"
