9ad7eb06e6fc574aeaa14142da020a53ed713054
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-07-10 22:54:44 -0600

    Added bind for OASIS-TRT-20_volumes

--- examples/launch_docker.sh
+++ examples/launch_docker.sh
@@ -5,4 +5,5 @@ docker run -it --rm --entrypoint=/bin/bash \
   -v /Users/kzoner/BBL/projects/ANTS/test_data/fmriprep/sub-93811/:/data/input/fmriprep \
   -v /Users/kzoner/BBL/projects/ANTS/test_data/singleSubjectTemplates/sub-93811:/data/output \
   -v /Users/kzoner/BBL/projects/ANTS/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/mindboggleVsBrainCOLOR_Atlases \
+#  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/OASIS-TRT-20_volumes \
   pennbbl/antssst -i
