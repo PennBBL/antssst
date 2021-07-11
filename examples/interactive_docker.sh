d7d6409a36154733f32ae437d196c1d2cb09be5d
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-07-11 08:11:35 -0600

    Renamed example scripts to interactive_container.sh

--- examples/interactive_docker.sh
+++ examples/interactive_docker.sh
@@ -4,6 +4,6 @@
 docker run -it --rm --entrypoint=/bin/bash \
   -v /Users/kzoner/BBL/projects/ANTS/test_data/fmriprep/sub-93811/:/data/input/fmriprep \
   -v /Users/kzoner/BBL/projects/ANTS/test_data/singleSubjectTemplates/sub-93811:/data/output \
-  -v /Users/kzoner/BBL/projects/ANTS/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/mindboggleVsBrainCOLOR_Atlases \
-#  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/OASIS-TRT-20_volumes \
+  -v /Users/kzoner/BBL/projects/ANTS/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
+#  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/atlases \
   pennbbl/antssst -i
--- examples/interactive_singularity.sh
+++ examples/interactive_singularity.sh
@@ -4,5 +4,5 @@
 singularity shell --cleanenv --writable-tmpfs --containall \
   -B ~/ants_pipelines/test_data/fmriprep/sub-93811:/data/input/fmriprep \
   -B ~/ants_pipelines/test_data/singleSubjectTemplates/sub-93811:/data/output \
-  -B ~/ants_pipelines/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/mindboggleVsBrainCOLOR_Atlases \
-  ~/ants_pipelines/images/antssst_0.0.8.sif
+  -B ~/ants_pipelines/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
+  ~/ants_pipelines/images/antssst_0.1.0.sif --seed 1 --jlf 
