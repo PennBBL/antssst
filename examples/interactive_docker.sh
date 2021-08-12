f563d65ce39afe4ab629a7a7abd17e040b916507
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-08-12 12:00:37 -0400

    Updated image version

--- examples/interactive_docker.sh
+++ examples/interactive_docker.sh
@@ -2,8 +2,8 @@
 
 # Run docker container interactively
 docker run -it --rm --entrypoint=/bin/bash \
-  -v /Users/kzoner/BBL/projects/ANTS/test_data/fmriprep/sub-93811/:/data/input/fmriprep \
-  -v /Users/kzoner/BBL/projects/ANTS/test_data/singleSubjectTemplates/sub-93811:/data/output \
-  -v /Users/kzoner/BBL/projects/ANTS/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
+  -v /Users/kzoner/BBL/projects/ANTS/data/fmriprep/sub-93811/:/data/input/fmriprep \
+  -v /Users/kzoner/BBL/projects/ANTS/data/singleSubjectTemplates/antssst-0.1.0/sub-93811:/data/output \
+  -v /Users/kzoner/BBL/projects/ANTS/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
+  katjz/antssst:0.1.0 -i
 #  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/atlases \
-  pennbbl/antssst -i
