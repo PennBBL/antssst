f80bf5855d8a63ba91226023af1a5295f2a46c41
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-09-13 10:20:07 -0400

    Updated singularity call, added non-interactive version.

--- examples/interactive_singularity.sh
+++ examples/interactive_singularity.sh
@@ -2,7 +2,14 @@
 
 # Run docker container interactively
 singularity shell --cleanenv --writable-tmpfs --containall \
-  -B ~/ants_pipelines/test_data/fmriprep/sub-93811:/data/input/fmriprep \
-  -B ~/ants_pipelines/test_data/singleSubjectTemplates/sub-93811:/data/output \
-  -B ~/ants_pipelines/test_data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
-  ~/ants_pipelines/images/antssst_0.1.0.sif --seed 1 --jlf 
+  -B ~/ants_pipelines/data/fmriprep/sub-93811:/data/input/fmriprep \
+  -B ~/ants_pipelines/data/ANTsLongitudinal/subjects/sub-93811:/data/output \
+  -B ~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
+  ~/ants_pipelines/images/antssst_0.1.0.sif
+
+# Run docker container (non-interactively)
+singularity run --cleanenv --writable-tmpfs --containall \
+  -B ~/ants_pipelines/data/fmriprep/sub-93811:/data/input/fmriprep \
+  -B ~/ants_pipelines/data/ANTsLongitudinal/subjects/sub-93811:/data/output \
+  -B ~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
+  ~/ants_pipelines/images/antssst_0.1.0.sif ses-PNC1 ses-PNC2 --seed 1 --jlf 
