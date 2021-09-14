3924f9df95cd135acc3f44e16750b467ffdaf5e7
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-09-14 14:08:25 -0400

    Script to launch antssst jobs on an entire project.

--- examples/launch_container.sh
+++ examples/launch_container.sh
@@ -1,14 +1,44 @@
-## Examples of Docker and Singularity commands to launch the ANTsSST 
-## container with the correct binding points.
+#!/bin/bash
 
-# Docker
-docker run --rm -ti --entrypoint="/bin/sh" \
-  -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-100088:/data/input \
-  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst5/sub-100088:/data/output \
-  pennbbl/antssst /scripts/run.sh ses-CONTE1 ses-NODRA1 ses-ONM1 ses-PNC1 ses-PNC2
+export LOGS_DIR=/home/kzoner/logs/ants/antssst-0.1.0
+mkdir -p ${LOGS_DIR}
 
-# Singularity
-singularity exec --writable-tmpfs --cleanenv \
-  -B /project/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-10410:/data/input \
-  -B /project/ExtraLong/data/singleSubjectTemplates/antssst/sub-10410:/data/output \
-  /project/ExtraLong/images/antssst_0.0.7.sif /scripts/run.sh ses-FNDM11 ses-FNDM21
+jsDir=~/ants_pipelines/scripts/jobscripts/antssst-0.1.0
+mkdir -p ${jsDir}
+
+fmriprep_dir=/project/ExtraLong/data/freesurferCrossSectional/fmriprep
+antslong_dir=~/ants_pipelines/data/ANTsLongitudinal
+atlas_dir=~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases
+
+gt_subs_csv=~/ants_pipelines/subjects_for_gt.csv
+exclude_csv=/project/ExtraLong/data/qualityAssessment/antssstExclude.csv
+
+subList=$(cat ${exclude_csv} | grep FALSE | cut -d , -f 1 | uniq)
+
+for subject in $subList; do
+
+  sessions=$(cat ${exclude_csv} | grep ${subject} | grep FALSE | cut -d \" -f 2 | sed "s/^/ses-/" | tr "\n" " ")
+  subject=sub-${subject}
+  echo SUBJECT: $subject
+  echo SESSIONS: $sessions
+
+  out_dir=${antslong_dir}/subjects/${subject}
+  mkdir -p ${out_dir}
+
+  jobscript=${jsDir}/${subject}.sh
+
+  cat <<-JOBSCRIPT >${jobscript}
+		#!/bin/bash
+	
+		singularity run --writable-tmpfs --cleanenv --containall \\
+			-B ${fmriprep_dir}/${subject}:/data/input/fmriprep \\
+			-B ${out_dir}:/data/output \\
+			-B ${atlas_dir}:/data/input/atlases \\
+			~/ants_pipelines/images/antssst_0.1.0.sif --seed 1 ${sessions}
+	
+	JOBSCRIPT
+
+  chmod +x ${jobscript}
+  bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
+
+done
