659b8025625a2e347510dea4de6b1ff2a6436031
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-09-14 14:06:57 -0400

    Changed image repo back to pennbbl.

--- examples/interactive_docker.sh
+++ examples/interactive_docker.sh
@@ -5,7 +5,7 @@ docker run -it --rm --entrypoint=/bin/bash \
   -v /Users/kzoner/BBL/projects/ANTS/data/fmriprep/sub-93811/:/data/input/fmriprep \
   -v /Users/kzoner/BBL/projects/ANTS/data/ANTsLongitudinal/0.1.0/subjects/sub-93811:/data/output \
   -v /Users/kzoner/BBL/projects/ANTS/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
-  katjz/antssst:0.1.0 -i
+  pennbbl/antssst:0.1.0 -i
 #  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/atlases \
 
 # Run docker container
@@ -13,5 +13,5 @@ docker run -it --rm \
   -v /Users/kzoner/BBL/projects/ANTS/data/fmriprep/sub-93811/:/data/input/fmriprep \
   -v /Users/kzoner/BBL/projects/ANTS/data/ANTsLongitudinal/0.1.0/subjects/sub-93811:/data/output \
   -v /Users/kzoner/BBL/projects/ANTS/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
-  katjz/antssst:0.1.0 ses-PNC1 ses-PNC2 --seed 1
+  pennbbl/antssst:0.1.0 ses-PNC1 ses-PNC2 --seed 1
 #  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/atlases \
