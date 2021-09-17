e2c1db40fdfd9435755e42ce4c6e04b55d2a69a1
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-09-17 12:45:54 -0400

    Added new cmd line args to example calls.

--- examples/interactive_docker.sh
+++ examples/interactive_docker.sh
@@ -13,5 +13,5 @@ docker run -it --rm \
   -v /Users/kzoner/BBL/projects/ANTS/data/fmriprep/sub-93811/:/data/input/fmriprep \
   -v /Users/kzoner/BBL/projects/ANTS/data/ANTsLongitudinal/0.1.0/subjects/sub-93811:/data/output \
   -v /Users/kzoner/BBL/projects/ANTS/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
-  pennbbl/antssst:0.1.0 ses-PNC1 ses-PNC2 --seed 1
+  pennbbl/antssst:0.1.0 ses-PNC1 ses-PNC2 --seed 1 --manual-step 1 -m 2 -m 4
 #  -v /Users/kzoner/BBL/projects/ANTS/test_data/OASIS-TRT-20_volumes:/data/input/atlases \
--- examples/interactive_singularity.sh
+++ examples/interactive_singularity.sh
@@ -2,14 +2,14 @@
 
 # Run docker container interactively
 singularity shell --cleanenv --writable-tmpfs --containall \
-  -B ~/ants_pipelines/data/fmriprep/sub-93811:/data/input/fmriprep \
+  -B /project/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-93811/:/data/input/fmriprep \
   -B ~/ants_pipelines/data/ANTsLongitudinal/subjects/sub-93811:/data/output \
   -B ~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
   ~/ants_pipelines/images/antssst_0.1.0.sif
 
 # Run docker container (non-interactively)
 singularity run --cleanenv --writable-tmpfs --containall \
-  -B ~/ants_pipelines/data/fmriprep/sub-93811:/data/input/fmriprep \
-  -B ~/ants_pipelines/data/ANTsLongitudinal/subjects/sub-93811:/data/output \
+  -B /project/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-93811:/data/input/fmriprep \
+  -B ~/ants_pipelines/data/test:/data/output \
   -B ~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases:/data/input/atlases \
-  ~/ants_pipelines/images/antssst_0.1.0.sif ses-PNC1 ses-PNC2 --seed 1 --jlf 
+  ~/ants_pipelines/images/antssst_0.1.0.sif ses-PNC1 ses-PNC2 --seed 1 -m 1
